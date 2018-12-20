6b8b829fc8c3103bba04f7a269db7c5a65b78fc0
Gilson Vieira <gilson.vieira@clickbus.com>
2018-12-20 13:26:04 -0200

    Headless mode

--- features/environment.py
+++ features/environment.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def before_all(context):
-    context.browser = webdriver.Chrome('/usr/local/bin/chromedriver')
+    options = Options()
+    options.add_argument('headless')
+    context.browser = webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=options)
+    
