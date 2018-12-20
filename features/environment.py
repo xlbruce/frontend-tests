2430bce109006ee29b7fe2149c8c64668d191948
Gilson Vieira <gilson.vieira@clickbus.com>
2018-12-20 13:56:48 -0200

    Update

--- features/environment.py
+++ features/environment.py
@@ -5,5 +5,5 @@ from webdriver_manager.chrome import ChromeDriverManager
 def before_all(context):
     options = Options()
     options.add_argument('headless')
-    context.browser = webdriver.Chrome(ChromeDriverManager.install(), chrome_options=options)
+    context.browser = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
     
